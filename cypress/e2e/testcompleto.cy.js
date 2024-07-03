cda52c6682ed18e39493827817e6bec5f93a7c0e
Kevin arciniegas <stickmarq@gmail.com>
2024-07-03 18:18:14 -0400

    Actiualizacion del test usando localizacion, navegacion y elementos

--- cypress/e2e/testcompleto.cy.js
+++ cypress/e2e/testcompleto.cy.js
@@ -7,4 +7,37 @@ describe('Test', ()=>{
         cy.visit('http://localhost:8888/#/')
     })
 
+    it('add 2 todos', ()=>{
+        cy.get('.new-todo')
+            .type('learn testing{enter}')
+            .type('be cool{enter}')
+        cy.get('.todo-list li').should('have.length', 2)
+    })
+
+    // context('Cuando la web este inicialmente abierta comprobar', ()=> {
+    //     cy.focused().should('have.class', 'new-todo')
+    // })
+    it('Comprobar localizadores atri, get, atri+tag, Id y class, ', ()=> {
+        cy.get('.info').contains('Double-click to edit a todo')
+        cy.get('[placeholder="What needs to be done?"]')
+        cy.get('input[placeholder="What needs to be done?"]')
+        // cy.get('#toggle-all').find('class').should('contains', 'Mark all as complete')
+        cy.get('.todoapp').find('.new-todo').get('input[placeholder="What needs to be done?"]')
+    })
+    it('Comprobar elementos contains y parents', ()=> {
+        cy.contains('TodoMVC')
+        cy.contains('.header.header', 'todos')
+        cy.get('.info').contains('Double-click to edit a todo')
+        cy.get('.new-todo')
+            .type('learn testing{enter}')
+                .type('be cool{enter}')
+        cy.get('.todoapp').find('.main').get('input[id="toggle-all"]').parents().find('label')
+        
+    })
+    it('Comprobar navegacion de la web', ()=>{
+        cy.reload()
+        cy.go('back')
+        cy.reload(true)
+
+    })
 })
